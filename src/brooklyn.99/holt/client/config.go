64a0dd3fc9f0b92fc388242c9f8b4770e94f4c6b
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-15 23:39:14 +0200

    Update config.go

--- src/brooklyn.99/holt/client/config.go
+++ src/brooklyn.99/holt/client/config.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"gopkg.in/ini.v1"
 )
@@ -13,14 +12,17 @@ func load_config() {
 	// Loading the config from the path `$CWD\config.ini`
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
-		write_log("init", fmt.Sprintf("Error loading config: %v\n", err))
+		write_log("config", fmt.Sprintf("Error loading config: %v\n", err))
 		os.Exit(1)
 	}
 
 	// Loading the `server` section from the config file
 	server_section_name := "server"
 	server_section, err := cfg.GetSection(server_section_name)
-	if err == nil {
+	if err != nil {
+		write_log("config", err.Error())
+		os.Exit(1)
+	} else {
 		config_server := make(map[string]interface{})
 
 		// The instance name of the server
@@ -40,25 +42,25 @@ func load_config() {
 		}
 
 		// The port the Holt server listens on
-		server_port, _ := strconv.Atoi(server_section.Key("port").String())
-		if server_port != 0 {
-			config_server["port"] = server_port
-		} else {
+		server_port, err := server_section.Key("port").Int()
+		if err != nil {
 			config_server["port"] = 8080
+		} else {
+			config_server["port"] = server_port
 		}
 
 		// The interval in which the client will contact Holt
-		server_interval, _ := strconv.Atoi(server_section.Key("interval").String())
-		if server_interval != 0 {
-			config_server["interval"] = server_interval
-		} else {
+		server_interval, err := server_section.Key("interval").Int()
+		if err != nil {
 			config_server["interval"] = 15
+		} else {
+			config_server["interval"] = server_interval
 		}
 
 		GLOBAL_CONFIG[server_section_name] = config_server
 	}
 
 	// Logs the loaded `server` configuration
-	write_log("init", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\tinstance_name = %s\n\thost = %s\n\tport = %d\n\tinterval = %d\n",
+	write_log("config", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\tinstance_name = %s\n\thost = %s\n\tport = %d\n\tinterval = %d\n",
 		"server", "server", GLOBAL_CONFIG["server"]["instance_name"], GLOBAL_CONFIG["server"]["host"], GLOBAL_CONFIG["server"]["port"], GLOBAL_CONFIG["server"]["interval"]))
 }
