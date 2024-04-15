abe16422485b3ff5e7dcdd9249dc06a37edfc6f6
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-15 23:19:12 +0200

    Update client.go

--- src/brooklyn.99/holt/client/client.go
+++ src/brooklyn.99/holt/client/client.go
@@ -5,59 +5,21 @@ import (
 	"os"
 )
 
-var global_client_hostname string
-var global_server_instance_name string
-var global_server_host string
-var global_server_port int
-var global_server_interval int
+var GLOBAL_CLIENT_HOSTNAME string
+var GLOBAL_CONFIG = make(map[string]map[string]interface{})
 
 func main() {
-
 	hostname, err := os.Hostname()
 	if err != nil {
 		write_log("init", fmt.Sprintf("Error getting hostname: %v\n", err))
 		os.Exit(1)
 	}
+	GLOBAL_CLIENT_HOSTNAME = hostname
 
-	global_client_hostname = hostname
-
-	/////////////////////////////////////////////////////////////////////////////////////
-	//                                                                                 //
-	//                                  Config                                         //
-	//                                                                                 //
-	/////////////////////////////////////////////////////////////////////////////////////
+	write_log("init", "Starting the client")
 
 	// Loading configuration from disk (implemented in config.go)
-	config, err := load_config()
-	var current_config_section string
-
-	if err != nil {
-		write_log("init", fmt.Sprintf("Error loading config: %v\n", err))
-		os.Exit(1)
-	}
-
-	// Loading the server section
-	current_config_section = "server"
-	if config[current_config_section] != nil {
-		global_server_instance_name = config[current_config_section]["instance_name"].(string)
-		global_server_host = config[current_config_section]["host"].(string)
-		global_server_port = config[current_config_section]["port"].(int)
-		global_server_interval = config[current_config_section]["interval"].(int)
-
-		// Logs the loaded `server` configuration
-		write_log("init", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\tinstance_name = %s\n\thost = %s\n\tport = %d\n\tinterval = %d\n",
-			current_config_section, current_config_section, global_server_instance_name, global_server_host, global_server_port, global_server_interval))
-	}
-
-	// Loading the server section
-	current_config_section = "client"
-	if config[current_config_section] != nil {
-		global_server_port = config[current_config_section]["port"].(int)
-
-		// Logs the loaded `client` configuration
-		write_log("init", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\tinstance_name = %s\n\thost = %s\n\tport = %d\n\tinterval = %d\n",
-			current_config_section, current_config_section, global_server_instance_name, global_server_host, global_server_port, global_server_interval))
-	}
+	load_config()
 
 	get("/help")
 }
