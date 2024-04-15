3a19c4f1029c2e583036b831acc656f9379636ca
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-15 22:38:28 +0200

    Providing default values to server section

--- src/brooklyn.99/holt/client/config.go
+++ src/brooklyn.99/holt/client/config.go
@@ -22,16 +22,37 @@ func load_config() (map[string]map[string]interface{}, error) {
 	if err == nil {
 		config_server := make(map[string]interface{})
 
+		// The instance name of the server
+		server_instance_name := server_section.Key("instance_name").String()
+		if server_instance_name != "" {
+			config_server["instance_name"] = server_instance_name
+		} else {
+			config_server["instance_name"] = "dev-holt.example.com"
+		}
+
 		// DNS or IP address of the Holt server
-		config_server["host"] = server_section.Key("host").String()
+		server_host := server_section.Key("host").String()
+		if server_host != "" {
+			config_server["host"] = server_host
+		} else {
+			config_server["host"] = "127.0.0.1"
+		}
 
 		// The port the Holt server listens on
 		server_port, _ := strconv.Atoi(server_section.Key("port").String())
-		config_server["port"] = server_port
+		if server_port != 0 {
+			config_server["port"] = server_port
+		} else {
+			config_server["port"] = 8080
+		}
 
 		// The interval in which the client will contact Holt
 		server_interval, _ := strconv.Atoi(server_section.Key("interval").String())
-		config_server["interval"] = server_interval
+		if server_interval != 0 {
+			config_server["interval"] = server_interval
+		} else {
+			config_server["interval"] = 15
+		}
 
 		config[server_section_name] = config_server
 	}
