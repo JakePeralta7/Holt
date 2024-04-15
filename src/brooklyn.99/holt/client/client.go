0ee550ab103a46092153eb68a398689722271d04
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-15 22:09:13 +0200

    Update client.go

--- src/brooklyn.99/holt/client/client.go
+++ src/brooklyn.99/holt/client/client.go
@@ -20,15 +20,35 @@ func main() {
 
 	global_client_hostname = hostname
 
+	/////////////////////////////////////////////////////////////////////////////////////
+	//                                                                                 //
+	//                                  Config                                         //
+	//                                                                                 //
+	/////////////////////////////////////////////////////////////////////////////////////
+
 	// Loading configuration from disk (implemented in config.go)
 	config, err := load_config()
+	var current_config_section string
+
 	if err != nil {
 		write_log("init", fmt.Sprintf("Error loading config: %v\n", err))
 		os.Exit(1)
 	}
 
-	// Printing the server section
-	current_config_section := "server"
+	// Loading the server section
+	current_config_section = "server"
+	if config[current_config_section] != nil {
+		global_server_host = config[current_config_section]["host"].(string)
+		global_server_port = config[current_config_section]["port"].(int)
+		global_server_interval = config[current_config_section]["interval"].(int)
+
+		// Logs the loaded `server` configuration
+		write_log("init", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\thost = %s\n\tport = %d\n\tinterval = %d\n",
+			current_config_section, current_config_section, global_server_host, global_server_port, global_server_interval))
+	}
+
+	// Loading the server section
+	current_config_section = "client"
 	if config[current_config_section] != nil {
 		global_server_host = config[current_config_section]["host"].(string)
 		global_server_port = config[current_config_section]["port"].(int)
