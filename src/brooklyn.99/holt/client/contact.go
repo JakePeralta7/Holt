6719ffde7b562d00b4a16e263b4ac52adeae1177
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-15 22:38:34 +0200

    Update contact.go

--- src/brooklyn.99/holt/client/contact.go
+++ src/brooklyn.99/holt/client/contact.go
@@ -9,8 +9,7 @@ import (
 func get(uri string) {
 	url := fmt.Sprintf("http://%s:%d%s", global_server_host, global_server_port, uri)
 
-	fmt.Printf("URL: %s\n",
-		url)
+	write_log("contact", fmt.Sprintf("Performing GET request to '%s'", url))
 
 	resp, err := http.Get(url)
 
