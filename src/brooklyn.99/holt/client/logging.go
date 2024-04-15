0ccc5bdfd9a95e5c84d7bebbf5c66cf1606b121f
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-15 21:23:27 +0200

    Update logging.go

--- src/brooklyn.99/holt/client/logging.go
+++ src/brooklyn.99/holt/client/logging.go
@@ -7,12 +7,14 @@ import (
 )
 
 func write_log(component string, message string) {
+
 	// Open a file for writing logs
 	file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Formatting the prefix for the line, for example "WIN10-HOST [init]:"
 	prefix := fmt.Sprintf("%s [%s]: ", global_client_hostname, component)
 
 	// Create a new logger
