99b0411da5b5a3769ef081b12b1a74d4b47cc9f3
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-04-17 23:22:37 +0200

    Update config.go

--- src/brooklyn.99/holt/server/config.go
+++ src/brooklyn.99/holt/server/config.go
@@ -65,4 +65,52 @@ func load_config() {
 	// Logs the loaded `server` configuration
 	write_log("config", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\tinstance_name = %s\n\thost = %s\n\tport = %d\n\tlog_dir = %s\n",
 		current_section_name, current_section_name, GLOBAL_CONFIG[current_section_name]["instance_name"], GLOBAL_CONFIG[current_section_name]["host"], GLOBAL_CONFIG[current_section_name]["port"], GLOBAL_CONFIG[current_section_name]["log_dir"]))
+
+	// Loading the `database` section from the config file
+	current_section_name = "database"
+	database_section, err := cfg.GetSection(current_section_name)
+	if err != nil {
+		write_log("config", err.Error())
+		os.Exit(1)
+	} else {
+		config_database := make(map[string]interface{})
+
+		// The instance name of the server
+		server_instance_name := database_section.Key("instance_name").String()
+		if server_instance_name != "" {
+			config_database["instance_name"] = server_instance_name
+		} else {
+			config_database["instance_name"] = "dev-holt.example.com"
+		}
+
+		// DNS or IP address of the Holt server
+		server_host := database_section.Key("host").String()
+		if server_host != "" {
+			config_database["host"] = server_host
+		} else {
+			config_database["host"] = "127.0.0.1"
+		}
+
+		// The port the Holt server listens on
+		server_port, err := database_section.Key("port").Int()
+		if err != nil {
+			config_database["port"] = 8080
+		} else {
+			config_database["port"] = server_port
+		}
+
+		// The directory in which the client.log will be stored
+		client_log_dir := database_section.Key("log_dir").String()
+		if client_log_dir != "" {
+			config_database["log_dir"] = client_log_dir
+		} else {
+			config_database["log_dir"] = "."
+		}
+
+		GLOBAL_CONFIG[current_section_name] = config_database
+	}
+
+	// Logs the loaded `server` configuration
+	write_log("config", fmt.Sprintf("Loading %s configuration:\n\t[%s]\n\tinstance_name = %s\n\thost = %s\n\tport = %d\n\tlog_dir = %s\n",
+		current_section_name, current_section_name, GLOBAL_CONFIG[current_section_name]["instance_name"], GLOBAL_CONFIG[current_section_name]["host"], GLOBAL_CONFIG[current_section_name]["port"], GLOBAL_CONFIG[current_section_name]["log_dir"]))
 }
